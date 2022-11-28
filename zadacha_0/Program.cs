bea01e956d2517df34724a0de7e62fd7ec6f3e62
alexns <alexns9419@yandex.ru>
2022-11-28 22:08:52 +0500

    zadacha_2

---
+++ zadacha_0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число : ");
+int number = int.Parse (Console.ReadLine());
+int square = number*number;
+Console.Write( $"квадрат чилса {number} равен {square}"); 
